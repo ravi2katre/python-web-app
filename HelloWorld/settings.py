a1baa9e50cc264696d2f5bbc6ac96c267ca749bc
ravi2katre <ravi2katre@gmail.com>
2018-07-09 09:31:50 +0530

    host commit

--- HelloWorld/settings.py
+++ HelloWorld/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'uefqx$ka)t6z7urfg$lkzr4z259+3=9!@0=$%%^^mv11y(ei3w'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['demo-helloworld.herokuapp.com']
 
 
 # Application definition
