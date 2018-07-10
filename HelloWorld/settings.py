ed3e6e1a841f4be4751007bee2a3ab903c00a612
ravi2katre <ravi2katre@gmail.com>
2018-07-10 21:34:38 +0530

    debug

--- HelloWorld/settings.py
+++ HelloWorld/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'uefqx$ka)t6z7urfg$lkzr4z259+3=9!@0=$%%^^mv11y(ei3w'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.limitless-anchorage-12972.herokuapp.com']
 
